e9681cf0e799c98f73fa3dc7df933e884bc33228
antoinebrias <antoinebrias@gmail.com>
2024-11-13 11:02:49 +0100

    renamed database.py to database_interface.py

--- src/database_interface.py
+++ src/database_interface.py
@@ -2,14 +2,23 @@ import sqlite3
 import pandas as pd
 import config
 from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def connect_db(db_path):
     """Establish a connection to the SQLite database."""
     return sqlite3.connect(db_path)
 
 
-def fetch_data(conn, bot_name, sample_size, history_depth, metrics_dict):
+def fetch_random_data(conn, bot, metrics_dict):
     """Fetch messages and historical context from the database for a specified bot."""
+
+    bot_name = bot['name']
+    sample_size = bot['sample_size']
+    history_depth = bot['history_depth']
+
     # SQL query to fetch random bot messages
     main_query = """
     SELECT m.id, m.conversation_id, m.content, m.ordinality, m.created_at
@@ -38,8 +47,6 @@ def fetch_data(conn, bot_name, sample_size, history_depth, metrics_dict):
         cursor.execute(context_query, (message_data['conversation_id'], message_data['ordinality'], history_depth))
         history_messages = cursor.fetchall()
 
-        question_timestamp = None # Initialize question_timestamp to None to handle missing case
-
         # Construct conversation context and question
         message_data['question'], message_data['conversation_context'], question_timestamp = construct_context(history_messages)
         data_with_history.append(message_data)
@@ -50,25 +57,17 @@ def fetch_data(conn, bot_name, sample_size, history_depth, metrics_dict):
         # Example of an Operational metric: response time
         for metric in metrics_dict["operational"]:
             if metric == "response_time":
-                response_timestamp = message_data['created_at']
-                message_data['response_time'] = 1 # Default response time of 1 second if no question timestamp is found
+                message_data['response_time'] = response_time(message_data,question_timestamp)
 
-                # Calculate response time if both timestamps exist
-                if question_timestamp and response_timestamp:
-                    # Convert to datetime objects if they are in string format
-                    question_timestamp = datetime.strptime(question_timestamp, "%Y-%m-%d %H:%M:%S") if isinstance(question_timestamp, str) else question_timestamp
-                    response_timestamp = datetime.strptime(response_timestamp, "%Y-%m-%d %H:%M:%S") if isinstance(response_timestamp, str) else response_timestamp
-                    
-                    # Calculate the time difference in seconds
-                    message_data['response_time'] = (response_timestamp - question_timestamp).total_seconds()
 
     return pd.DataFrame(data_with_history)
 
 
 def construct_context(history_messages):
-    """Create question and context strings from message history."""
+    """Create question, context strings and question timestamp from message history."""
     question = ""
     conversation_context = ""
+    question_timestamp = None # Initialize question_timestamp to None to handle missing case
     for msg in history_messages:
         label = "Chatbot- " if msg[1] == 1 else "User- "
         if msg[1] == 0 and not question:
@@ -77,3 +76,22 @@ def construct_context(history_messages):
         conversation_context += f"{label} {msg[0]}\n"
     return question, conversation_context.strip(),question_timestamp
 
+def convert_timestamp(time_stamp):
+    return datetime.strptime(time_stamp, "%Y-%m-%d %H:%M:%S") if isinstance(time_stamp, str) else time_stamp
+
+
+def response_time(message_data,question_timestamp):
+    response_timestamp = message_data['created_at']
+    message_data['response_time'] = 1 # Default response time of 1 second if no question timestamp is found
+
+    # Calculate response time if both timestamps exist
+    if question_timestamp and response_timestamp:
+        # Convert to datetime objects if they are in string format
+        try:
+            question_timestamp = convert_timestamp(question_timestamp)
+            response_timestamp = convert_timestamp(response_timestamp)
+            # Calculate the time difference in seconds
+            return (response_timestamp - question_timestamp).total_seconds()
+        except:
+            logging.error(f"Could not process timestamp for id: {message_data['conversation_id']}")
+        
